6d35a4866a1f36f09890b9966fe85f7be8d54756
helodeepakji <helodeepakji@gmail.com>
2023-05-21 15:24:20 +0530

    api request send to django

--- home/views.py
+++ home/views.py
@@ -5,3 +5,19 @@ def index(request):
 
 def about(request):
     return render(request,'index.html')
+
+def regisiter(request):
+    if request.method == 'POST':
+        name = request.POST.get('name')
+        email = request.POST.get('email')
+        # Retrieve other form fields
+
+        # Create a new instance of the model and save the data
+        my_form_data = MyFormData(name=name, email=email)
+        my_form_data.save()
+
+        # Return a JSON response indicating success
+        return JsonResponse({'message': 'Form data saved successfully'})
+
+    # Return an error response if the request method is not POST
+    return render(request,'index.html')
