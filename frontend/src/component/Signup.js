fc17c5479f05aca7776f3bf2d8cb327831dd5e68
Shivani Singh <shivanisingh36813@gmail.com>
2023-05-21 15:02:54 +0530

    login page committed

--- frontend/src/component/Signup.js
+++ frontend/src/component/Signup.js
@@ -2,21 +2,7 @@ import React from 'react'
 
 const Signup = () => {
   return (
-    <div className='s1'>
-        <div className='s2'>
-                    <form>
-                        <div className='s2'>
-                        <label>Username</label>
-                        <input type='text' placeholder='Username' required></input>
-                        </div>
-
-                        <div className='s2'>
-                        <label>Password</label>
-                        <input type='password' placeholder='Password' required></input>
-                        </div>
-                    </form>
-        </div>
-    </div>
+    <div>Signup</div>
   )
 }
 
