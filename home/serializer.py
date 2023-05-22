a1e255f340669501bb48ab59df4c7059c996c5c0
Tarun Garg <tanugarg1234567@gmail.com>
2023-05-23 03:28:51 +0530

    minors

--- home/serializer.py
+++ home/serializer.py
@@ -3,15 +3,24 @@ from rest_framework import serializers
 from .models import *
 
 class UserSerializer(serializers.ModelSerializer):
+    
     class Meta:
         model = User
-        fields = '__all__'
+        fields = {'username','password','first_name','last_name','Profile_pic','Gender','Is_worker','Phone_number'}
+        read_only_fields = ['password']
+    # returns true if user is worker
+    def get_is_worker(self):
+        return self.Is_worker
 
 class AddressSerializer(serializers.ModelSerializer):
     class Meta:
         model = Address
-        fields = '__all__'
+        fields = {'Area','City','State','Pincode','Default'}
 class WorkerSerializer(serializers.ModelSerializer):
     class Meta:
         model = Worker
-        fields = '__all__'
+        fields = {'Education','Age','Category','Gov_id','Salary_type','UserID'}
+    
+    def create(self, validated_data):
+        print(validated_data)
+        Worker
--- home/views.py
+++ home/views.py
@@ -2,6 +2,8 @@ import json
 from django.shortcuts import render
 from django.http import JsonResponse
 from django.middleware.csrf import get_token
+from rest_framework import generics
+# from .serializer import *
 
 def get_csrf_token(request):
     csrf_token = get_token(request)
@@ -19,5 +21,11 @@ def regisiter(request):
         data = json.loads(request.body)
         print(data)
         return JsonResponse({'message': 'Form data saved successfully'})
-
     return render(request,'index.html')
+
+class register(generics.CreateAPIView):
+    def get(self,request):
+        return render(request,'index.html')
+    def post(self,request):
+        
+        return JsonResponse({'message': 'Form data saved successfully'})
