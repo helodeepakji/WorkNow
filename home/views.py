0beedbccfa7e4ad2d154c0005193246a1e64e3a2
Tarun Garg <tanugarg1234567@gmail.com>
2023-05-23 18:32:26 +0530

    minors

--- home/views.py
+++ home/views.py
@@ -27,5 +27,35 @@ class register(generics.CreateAPIView):
     def get(self,request):
         return render(request,'index.html')
     def post(self,request):
-        
+        data = request.data
+        first_name = data['name'].split(' ')[0]
+        if len(data['name'].split(' ')) > 1:
+            last_name = data['name'].split(' ')[1]
+        else:
+            last_name = ''
+        phone = data['phone']
+        education = data['education']
+        gender = data['gender']
+        age = data['age']
+        password = data['password']
+        cpassword = data['cpassword']
+        category = data['category']
+        language = data['language']
+        area = data['area']
+        city = data['city']
+        state = data['state']
+        pincode = data['pincode']
+        salarytype = data['salarytype']
+        salary = data['salary']
+        govidtype = data['govidtype']
+        govid = data['govid']
+        profile_pic = data['profile']
+        user_data = {
+            'first_name': first_name,
+            'last_name': last_name,
+            'username': 'email',
+        }
+        print(data)
         return JsonResponse({'message': 'Form data saved successfully'})
+
+'''{'name': '', 'phone': '', 'education': '', 'gender': '', 'age': '', 'password': '', 'cpassword': '', 'category': 'Carpenter', 'language': '', 'area': '', 'city': '', 'state': '', 'pincode': '', 'salarytype': 'Daily Basis', 'salary': '', 'govidtype': 'Aadhar', 'govid': '', 'profile': None}'''
