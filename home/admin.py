5d6dbe1bd295d106b7fa23a5034077349a94534f
Tarun Garg <tanugarg1234567@gmail.com>
2023-05-24 00:50:44 +0530

    worker registration and models modification

--- home/admin.py
+++ home/admin.py
@@ -3,4 +3,5 @@ from .models import *
 # Register your models here.
 admin.site.register(Worker)
 admin.site.register(Address)
-admin.site.register(User)
+admin.site.register(Auth)
+admin.site.register(Customer)
--- home/views.py
+++ home/views.py
@@ -3,7 +3,7 @@ from django.shortcuts import render
 from django.http import JsonResponse
 from django.middleware.csrf import get_token
 from rest_framework import generics
-# from .serializer import *
+from .serializer import *
 
 def get_csrf_token(request):
     csrf_token = get_token(request)
@@ -27,35 +27,57 @@ class register(generics.CreateAPIView):
     def get(self,request):
         return render(request,'index.html')
     def post(self,request):
-        data = request.data
-        first_name = data['name'].split(' ')[0]
-        if len(data['name'].split(' ')) > 1:
-            last_name = data['name'].split(' ')[1]
-        else:
-            last_name = ''
-        phone = data['phone']
-        education = data['education']
-        gender = data['gender']
-        age = data['age']
-        password = data['password']
-        cpassword = data['cpassword']
-        category = data['category']
-        language = data['language']
-        area = data['area']
-        city = data['city']
-        state = data['state']
-        pincode = data['pincode']
-        salarytype = data['salarytype']
-        salary = data['salary']
-        govidtype = data['govidtype']
-        govid = data['govid']
-        profile_pic = data['profile']
-        user_data = {
-            'first_name': first_name,
-            'last_name': last_name,
-            'username': 'email',
+        # send data to backend in this format.
+        temp_data = {
+            'Auth':{
+                'username': '9988776660',
+                'password': 'test',
+                'cpassword': 'test',
+                'Profile_pic': None,
+                'Name': 'test',
+                'Is_worker': True,
+                'Is_customer': False,
+                'Gender': 'M',
+            },
+            'Worker':{
+                'Category': 'Carpenter',
+                'Language': 'Hindi',
+                'Salary_type': 'D_B',
+                'Salary': 500,
+                'Govid_type': 'Aadhar',
+                'Gov_id': '2564787898',
+                'Education': 'B',
+                'Age': '20',
+            },
+            'Address':{
+                'Area': 'test',
+                'City': 'test',
+                'State': 'test',
+                'Pincode': 'test',
+                'Default': True,
+            }
         }
-        print(data)
+        
+        temp_data_auth = temp_data['Auth']
+        print('temp_data_auth',temp_data_auth)
+        temp_data_worker = temp_data['Worker']
+        print('temp_data_worker',temp_data_auth)
+        temp_data_address = temp_data['Address']
+        print('temp_data_address',temp_data_auth)
+        registerAuth = RegisterAuthSerializer(data=temp_data_auth)
+        registerAuth.is_valid(raise_exception=True)
+        registerAuth.save()
+        temp_data_worker['Auth_ID'] = registerAuth.get_id()
+        print("registerAuth_data",registerAuth.data)
+        registerWorker = WorkerSerializer(data=temp_data_worker)
+        if registerWorker.is_valid(raise_exception=True):
+            print('valid')
+        registerWorker.save()
+        print(registerWorker.get_id())
+        temp_data_address['WorkerID'] = registerWorker.get_id()
+        registerAddress = AddressSerializer(data=temp_data_address)
+        registerAddress.is_valid(raise_exception=True)
+        registerAddress.save()
         return JsonResponse({'message': 'Form data saved successfully'})
 
 '''{'name': '', 'phone': '', 'education': '', 'gender': '', 'age': '', 'password': '', 'cpassword': '', 'category': 'Carpenter', 'language': '', 'area': '', 'city': '', 'state': '', 'pincode': '', 'salarytype': 'Daily Basis', 'salary': '', 'govidtype': 'Aadhar', 'govid': '', 'profile': None}'''
